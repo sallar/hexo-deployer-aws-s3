584ad9e7a9243e0008b54f8814b306a10d4613b3
Sallar Kaboli <sallar@kiosked.com>
2016-03-22 16:04:39 +0200

    Added deployer code

--- src/deployer.js
+++ src/deployer.js
@@ -1,9 +1,55 @@
 (function(module) {
     "use strict";
 
+    var s3 = require("s3");
+
     module.exports = function(args) {
-        console.log(this.config);
-        console.log(args);
+
+        /* Setup S3 Config */
+        var credentials = new s3.AWS.SharedIniFileCredentials({
+            profile: args.profile || "default"
+        });
+        s3.AWS.config.credentials = credentials;
+        s3.AWS.config.update({
+            region: args.region
+        });
+
+        /* Create the S3 Client */
+        var client = s3.createClient({
+            maxAsyncS3: 20,
+            s3RetryCount: 3,
+            s3RetryDelay: 1000,
+            s3Options: {
+                // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
+            },
+        });
+
+        /* Upload Config */
+        var params = {
+            localDir: this.config.public_dir,
+            deleteRemoved: true,
+
+            s3Params: {
+                Bucket: args.bucket,
+                Prefix: args.prefix || ""
+            }
+        };
+
+        /* Create the uploader */
+        return new Promise(function(resolve, reject) {
+            var uploader = client.uploadDir(params);
+
+            uploader.on("error", function(err) {
+                console.error("unable to sync:", err.stack);
+                reject(err.message);
+            });
+            uploader.on("progress", function() {
+                console.log("progress", uploader.progressMd5Amount, uploader.progressMd5Total);
+            });
+            uploader.on("end", function() {
+                resolve();
+            });
+        });
     };
 
 })(module);
